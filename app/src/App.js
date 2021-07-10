533b5b1919b567dbf4099a74a1e98e205b4600c3
sabrinaspage <sabreyes01@gmail.com>
2021-07-10 15:53:52 -0400

    reroute to login

--- app/src/App.js
+++ app/src/App.js
@@ -8,6 +8,7 @@ import {
 import SignUpPage from "./pages/SignUpPage";
 import LeaderBoard from "./pages/LeaderBoard/index";
 import ChooseACharacter from "./pages/ChooseACharacter/index";
+import LogInPage from "./pages/LogInPage";
 import Game from "./game"
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -17,6 +18,7 @@ const App = () => {
       <Switch>
         <Route exact component={SignUpPage} path="/"/>
         <Route exact component={ChooseACharacter} path="/choose-a-character"/>
+        <Route exact component={LogInPage} path="/login"/>
         <Route exact component={Game} path="/game"/>
         <Route exact component={LeaderBoard} path="/leaderboard"/>
       </Switch>
