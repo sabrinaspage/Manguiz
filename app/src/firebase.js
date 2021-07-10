ae968d6fb898e8aefc743a1e00ceec855c6f68d8
sabrinaspage <sabreyes01@gmail.com>
2021-07-10 00:17:32 -0400

    implement firebase for leaderboard

---
+++ app/src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCCA5OGOp3j3TQBw5b87ByLtP6Xzi3MfDU",
+    authDomain: "hacktoon-d85b6.firebaseapp.com",
+    databaseURL: "https://hacktoon-d85b6-default-rtdb.firebaseio.com",
+    projectId: "hacktoon-d85b6",
+    storageBucket: "hacktoon-d85b6.appspot.com",
+    messagingSenderId: "132924006034",
+    appId: "1:132924006034:web:cc844b175134d5d547904b",
+    measurementId: "G-EPB4GEZLH9"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
