a4976eb17a487c2b4205f9d28d3c663256a9350b
sabrinaspage <sabreyes01@gmail.com>
2021-07-10 03:16:54 -0400

    route to page

--- app/src/App.js
+++ app/src/App.js
@@ -7,12 +7,14 @@ import {
 } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import LeaderBoard from "./pages/LeaderBoard/LeaderBoard";
+import Game from "./game"
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact component={SignUpPage} path="/"/>
+        <Route exact component={Game} path="/game"/>
         <Route exact component={LeaderBoard} path="/leaderboard"/>
       </Switch>
     </Router>
