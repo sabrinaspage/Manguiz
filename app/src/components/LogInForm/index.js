64e6169d7f4127f990acbd26d77adfadd708c3f6
sabrinaspage <sabreyes01@gmail.com>
2021-07-10 16:20:10 -0400

    matching border for login, logout

--- app/src/components/LogInForm/index.js
+++ app/src/components/LogInForm/index.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../../contexts/AuthContext";
 import { useHistory, Link } from "react-router-dom";
+import "./index.css";
 
 export default function LogInForm() {
   const emailRef = useRef();
@@ -28,7 +29,7 @@ export default function LogInForm() {
 
   return (
     <>
-      <Card>
+      <Card className="form-decor">
         <Card.Body>
           <h2 className="text-center mb-4">Log In</h2>
           {error && <Alert variant="danger">{error}</Alert>}
